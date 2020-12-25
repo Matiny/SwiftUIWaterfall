fce7af6c6211408c434678259a37d341839f6953
Mat Louis <mattymat@Mats-Mac.local>
2020-12-25 14:30:03 -0800

    Adding project

---
+++ Waterfall/WaterfallApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterfallApp.swift
+//  Waterfall
+//
+//  Created by Mat Louis on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct WaterfallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
